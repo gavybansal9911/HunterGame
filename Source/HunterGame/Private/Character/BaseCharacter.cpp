998cfa16bbf30068553eb32b6fb28df363a079e3
Shinchan <gavybansal9911@gmail.com>
2023-11-01 11:59:14 +0530

    Basic Character Movement Setup with Enhanced Input System

--- Source/HunterGame/Private/Character/BaseCharacter.cpp
+++ Source/HunterGame/Private/Character/BaseCharacter.cpp
@@ -2,15 +2,42 @@
 
 
 #include "Character/BaseCharacter.h"
+#include "EnhancedInputSubsystems.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "InputAction.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 ABaseCharacter::ABaseCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Boom"));
+	CameraBoom->SetupAttachment(GetMesh());
+	CameraBoom->TargetArmLength = 400.f;
+
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("View Camera"));
+	ViewCamera->SetupAttachment(CameraBoom);
+	
+	bUseControllerRotationPitch = true;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 void ABaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (const APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* EnhancedInputLocalPlayerSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			EnhancedInputLocalPlayerSubsystem->AddMappingContext(BaseInputMappingContext, 1);
+		}
+	}
 }
 
 void ABaseCharacter::Tick(float DeltaTime)
@@ -18,7 +45,52 @@ void ABaseCharacter::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+void ABaseCharacter::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+}
+
 void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered,this, &ABaseCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABaseCharacter::Look);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ABaseCharacter::Jump);
+	}
+}
+
+void ABaseCharacter::Move(const FInputActionValue& Value)
+{
+	if (!Controller) return;
+
+	const FVector2d InputValue = Value.Get<FVector2d>();
+	const FRotator YawRotation = FRotator(0.f, GetControlRotation().Yaw, 0.f);
+	if (InputValue.Y != 0.f)
+	{
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(ForwardDirection, InputValue.Y);
+	}
+	if (InputValue.X != 0.f)
+	{
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(RightDirection, InputValue.X);
+	}
+}
+
+void ABaseCharacter::Look(const FInputActionValue& Value)
+{
+	if (!Controller) return;
+
+	const FVector2d InputValue = Value.Get<FVector2d>();
+	if (InputValue.X != 0.f) {AddControllerYawInput(InputValue.X);}
+	if (InputValue.Y != 0.f) {AddControllerPitchInput(InputValue.Y);}
+}
+
+void ABaseCharacter::Jump()
+{
+	if (!Controller) return;
+	ACharacter::Jump();
 }
--- Source/HunterGame/Public/Character/BaseCharacter.h
+++ Source/HunterGame/Public/Character/BaseCharacter.h
@@ -4,7 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "BaseCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class HUNTERGAME_API ABaseCharacter : public ACharacter
@@ -14,8 +20,38 @@ class HUNTERGAME_API ABaseCharacter : public ACharacter
 public:
 	ABaseCharacter();
 	virtual void Tick(float DeltaTime) override;
+	virtual void PostInitializeComponents() override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
 protected:
 	virtual void BeginPlay() override;
+
+	/** Input CallBacks **/
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	virtual void Jump() override;
+	/** Input CallBacks **/
+	
+	/** Input **/
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> BaseInputMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> JumpAction;
+	/** Input **/
+
+private:
+	/** Camera **/
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	TObjectPtr<USpringArmComponent> CameraBoom;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	TObjectPtr<UCameraComponent> ViewCamera;
+	/** Camera **/
 };
