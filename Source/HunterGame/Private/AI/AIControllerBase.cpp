508ac78e5b662b65ac638f05197c33f475da9c00
Shinchan <gavybansal9911@gmail.com>
2023-11-22 08:43:06 +0530

    Configure AI Perception Properties

--- Source/HunterGame/Private/AI/AIControllerBase.cpp
+++ Source/HunterGame/Private/AI/AIControllerBase.cpp
@@ -14,14 +14,33 @@ AAIControllerBase::AAIControllerBase()
 {
 	Blackboard = CreateDefaultSubobject<UBlackboardComponent>(TEXT("Blackboard Component"));
 	BehaviorTree = CreateDefaultSubobject<UBehaviorTree>(TEXT("Behavior Tree"));
+
 	PerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("Perception Component"));
+
+	FAISenseAffiliationFilter AISenseAffiliationFilter;
+	AISenseAffiliationFilter.bDetectEnemies = true;
+	AISenseAffiliationFilter.bDetectFriendlies = true;
+	AISenseAffiliationFilter.bDetectNeutrals = true;
+	
 	SenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("AI Sight Config"));
-	SenseConfig_Hearing = CreateDefaultSubobject<UAISenseConfig_Hearing>(TEXT("AI Hearing Config"));
-	SenseConfig_Damage = CreateDefaultSubobject<UAISenseConfig_Damage>(TEXT("AI Damage Config"));
 	PerceptionComponent->ConfigureSense(*SenseConfig_Sight);
+	SenseConfig_Sight->SightRadius = 2500.f;
+	SenseConfig_Sight->LoseSightRadius = 4000.f;
+	SenseConfig_Sight->PeripheralVisionAngleDegrees = 65.f;
+	SenseConfig_Sight->DetectionByAffiliation = AISenseAffiliationFilter;
+	SenseConfig_Sight->SetMaxAge(5.f);
+
+	SenseConfig_Hearing = CreateDefaultSubobject<UAISenseConfig_Hearing>(TEXT("AI Hearing Config"));
 	PerceptionComponent->ConfigureSense(*SenseConfig_Hearing);
+	SenseConfig_Hearing->DetectionByAffiliation = AISenseAffiliationFilter;
+	SenseConfig_Hearing->HearingRange = 1500.f;
+	SenseConfig_Hearing->SetMaxAge(3.f);
+
+	SenseConfig_Damage = CreateDefaultSubobject<UAISenseConfig_Damage>(TEXT("AI Damage Config"));
 	PerceptionComponent->ConfigureSense(*SenseConfig_Damage);
-	PerceptionComponent->SetDominantSense(SenseConfig_Damage->GetSenseImplementation());
+	SenseConfig_Damage->SetMaxAge(6.5f);
+
+	PerceptionComponent->SetDominantSense(SenseConfig_Damage->GetSenseImplementation());  // Dominant Sense
 }
 
 void AAIControllerBase::PostInitializeComponents()
