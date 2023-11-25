f404a5d40fc97205e0b5be38f378032b20bf7666
Shinchan <gavybansal9911@gmail.com>
2023-11-25 14:09:26 +0530

    Inventory widgets setup

---
+++ Source/HunterGame/Public/HUD/InventoryUI/InventoryMenuUW.h
@@ -0,0 +1,17 @@
+// Hunter Game by Gamer Guru.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HUD/UserWidgetBase.h"
+#include "InventoryMenuUW.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HUNTERGAME_API UInventoryMenuUW : public UUserWidgetBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/HunterGame/Public/HUD/UserWidgetBase.h
@@ -0,0 +1,17 @@
+// Hunter Game by Gamer Guru.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UserWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HUNTERGAME_API UUserWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
