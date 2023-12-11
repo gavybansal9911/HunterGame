e6a57adadb96be935491592d4faa4a401a5b19f5
Gavish Bansal <gavybansal9911@gmail.com>
2023-12-11 14:28:50 +0530

    Minor changes

--- Source/HunterGame/Private/AI/Task/BTTGetRandomLocationInArea.cpp
+++ Source/HunterGame/Private/AI/Task/BTTGetRandomLocationInArea.cpp
@@ -8,18 +8,12 @@
 
 EBTNodeResult::Type UBTTGetRandomLocationInArea::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	GET_NEW_POINT:
 	if (UBlackboardComponent* BlackboardComponent = OwnerComp.GetBlackboardComponent())
 	{
 		FVector Origin = OwnerComp .GetAIOwner()->GetPawn()->GetActorLocation();
 		FVector Local_PointOfInterest;
 		UNavigationSystemV1::K2_GetRandomLocationInNavigableRadius(GetWorld(), Origin,
 			Local_PointOfInterest, NavigableAreaRadius);
-
-		if (OwnerComp.GetAIOwner()->GetPawn()->GetActorLocation().Size() - Local_PointOfInterest.Size() < 150.f)
-		{
-			goto GET_NEW_POINT;
-		}
 
 		BlackboardComponent->SetValueAsVector(BB_PointOfInterest_KeyName, Local_PointOfInterest);
 
