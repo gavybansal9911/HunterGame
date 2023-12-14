4c74292bde739f46f10859e6ea7aeaf019964c92
Gavish Bansal <gavybansal9911@gmail.com>
2023-12-14 14:04:37 +0530

    Animal attributes basic setup

--- Source/HunterGame/Private/Component/AnimalSurvivalComponent.cpp
+++ Source/HunterGame/Private/Component/AnimalSurvivalComponent.cpp
@@ -12,3 +12,27 @@ void UAnimalSurvivalComponent::BeginPlay()
 {
 	Super::BeginPlay();
 }
+
+void UAnimalSurvivalComponent::Init_Attributes()
+{
+	Init_Health();
+	Init_Stamina();
+
+	OnAttributesSet();
+}
+
+void UAnimalSurvivalComponent::Init_Health()
+{
+	MaxHealth = 100.f;
+	Health = 100.f;
+}
+
+void UAnimalSurvivalComponent::Init_Stamina()
+{
+	MaxStamina = 100.f;
+	Stamina = 100.f;
+}
+
+void UAnimalSurvivalComponent::OnAttributesSet()
+{
+}
--- Source/HunterGame/Public/Component/AnimalSurvivalComponent.h
+++ Source/HunterGame/Public/Component/AnimalSurvivalComponent.h
@@ -20,12 +20,22 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void Init_Attributes();
+	virtual void Init_Health();
+	virtual void Init_Stamina();
+	virtual void OnAttributesSet();
+
+	// Attributes
+	float Health;
+	float MaxHealth;
+	float Stamina;
+	float MaxStamina;
 
 private:
 	// Owner Reference
 	UPROPERTY()
 	TObjectPtr<ABaseAnimal> OwnerAnimalCharacter;
-
+	
 	// Behavior factors  =>  All values will be in a mapped range of [0, 1];
 	float AggressionFactor;
 	float FearFactor;
